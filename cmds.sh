cb4ef6c0f1f50e86fdc91bc553cfb0715b874dc6
meliposen <torstein.leversund@stud.hib.no>
2017-09-26 13:00:15 +0200

    Update cmds.sh

--- cmds.sh
+++ cmds.sh
@@ -4,3 +4,16 @@ ln -s ./Dropbox/00_hvl/dat157/ dat157
 
 # convert from html to pdf
  wkhtmltopdf perceptron.html perceptron2.pdf
+
+
+# installere jupyter
+
+sudo apt-get install python3-pip
+
+pip3 install numpy
+pip3 install pandas
+pip3 install scikit-learn
+pip3 install matplotlib
+pip3 install jupyter
+pip3 install python-config
+# evt ipython3 profile create 
